238bb31355435cb88176e1c535deb2cc31c0ede5
zhoukuo123 <1178053078@qq.com>
2020-12-31 12:55:13 +0800

    update

---
+++ MazeSolver/src/com/zk/Main.java
@@ -0,0 +1,9 @@
+package com.zk;
+
+public class Main {
+    public static void main(String[] args) {
+        String mazeFile = "/home/linux/IdeaProjects/LearnJava/MazeSolver/src/com/zk/maze_101_101.txt";
+        MazeData data = new MazeData(mazeFile);
+        data.print();
+    }
+}
